9fb66d261fa31061cd5d3532de88c596a3cd6ebd
indraaristya <indraaristya98@gmail.com>
2018-05-23 20:12:48 +0700

    first commit

--- Scala/HelloWorld/src/Test.scala
+++
@@ -1,6 +0,0 @@
-class Test(y: Int) {
-  private var x = y
-
-  def getX = x
-
-}
--- Scala/src/main/scala/example/Main.scala
+++
@@ -1,6 +0,0 @@
-package example
-
-object Main extends App{
-  val ages = Seq(12,90,99)
-  println(s"The oldest is ${ages.max}")
-}
