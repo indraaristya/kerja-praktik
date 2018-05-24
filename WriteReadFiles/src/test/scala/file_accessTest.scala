aad06b670988b24d2ba4e261e1f6281de690e55c
indraaristya <indraaristya98@gmail.com>
2018-05-24 15:45:52 +0700

    update test

--- WriteReadFiles/src/test/scala/file_accessTest.scala
+++ WriteReadFiles/src/test/scala/file_accessTest.scala
@@ -1,9 +1,9 @@
 import org.scalatest._
-class file_accessTest extends FlatSpec {
+class file_accessTest extends FlatSpec with Matchers {
   "readfile" should "return a same value with " in {
     val accs_file = new file_access
     accs_file.readfile("testWrite.txt")
     val hasil = "Ini adalah text yang dicoba untuk di write"
-      assert(accs_file.text === hasil)
+    (accs_file.text) should be (hasil)
   }
 }
